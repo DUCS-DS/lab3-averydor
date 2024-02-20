b99aaf53e534fca41a0389f5f37122c6bc846030
averydor <averydor@gmail.com>
2024-02-20 10:30:56 -0600

    finished length and print

--- finite.py
+++ finite.py
@@ -3,18 +3,34 @@ from llist import LList, Node, append
 
 def length(lst):
     """return the length of a finite linked list"""
-    pass  # delete this line when you add your code
-
+    counter = 0
+    current = lst.head
+    while current:
+        counter += 1
+        current = current.next
+    return counter
 
 def llprint(lst):
     """print a finite linked list"""
-    pass  # delete this line when you add your code
+    current = lst.head
+    while current:
+        print(current.val, end=" ")
+        current = current.next
+    print()
+
 
 
 if __name__ == "__main__":
-
-    pass  # delete this line when you add your code below
-
-    #
-    # your tests go here
-    #
+    llist = LList()
+    append(llist, Node(1))
+    append(llist, Node(2))
+    append(llist, Node(3))
+    append(llist, Node(8))
+    append(llist, Node(16))
+    append(llist, Node(32))
+    append(llist, Node(64))
+    append(llist, Node(128))
+    append(llist, Node(256))
+    append(llist, Node(512))
+    print(length(llist))
+    llprint(llist)
